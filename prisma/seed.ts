c1ef4780211287e312201aac9f34f0df5580b7ec
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-11-01 21:03:45 -0300

    Create db seed population

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john_doe@gmail.com",
+      avatarUrl: "https://github.com/leamsa.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example pool",
+      code: "BOL123",
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.match.create({
+    data: {
+      date: "2022-11-02T12:00:00.201Z",
+      homeTeamCountryCode: "DE",
+      awayTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.match.create({
+    data: {
+      date: "2022-11-03T12:00:00.201Z",
+      homeTeamCountryCode: "BR",
+      awayTeamCountryCode: "AR",
+
+      bets: {
+        create: {
+          homeTeamGoals: 2,
+          awayTeamGoals: 0,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
